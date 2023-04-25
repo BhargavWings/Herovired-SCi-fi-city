d011f6110dbb0263f9b16dd4962ec1f818dd4910
Bhargav Fofandi <bhargav.f@wingsts.com>
2023-04-25 13:31:26 +0530

    first commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1718488.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 129249510, 129249498, 129272656, 129283192, 129692823, 129893577, 130598137, 130651262 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/129470673/1/draco.wasm.js', 'wasmUrl' : 'files/assets/129470674/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/129470672/1/draco.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/129249477/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/129249472/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/129249483/1/ammo.js', 'preload' : true},
+];
